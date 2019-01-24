4d94311b32f29afbd57e18cf040998b20e841392
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-24 04:43:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,10 +3,9 @@ my_new_array = []
 end
 
 def array_with_two_elements
-  my_new_array = [1, 2]
+  my_new_array = ["Welcome to New York", 2]
 end
 
 def first_element(array)
-  my_new_array = ["Welcome to New York", 2, 3]
 array_with_two_elements[0]
 end
